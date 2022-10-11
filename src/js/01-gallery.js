2aced02c722adbd557080941a7a1471cd6df6698
Anastasiia <pavlishinka12@icloud.com>
2022-10-11 18:16:42 +0300

    do first hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+ import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+//console.log(galleryItems);
+
+
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+function createGallery(items) {
+    return items.map((item) => `
+    <a class="gallery__item" href= ${item.original}>
+  <img class="gallery__image" src= ${item.preview} alt=${item.description} />
+</a>`).join('')
+}
+const newCreat = createGallery(galleryItems);
+gallery.innerHTML = newCreat;
+
+
+gallery.addEventListener('click', imageClick);
+
+function imageClick(event) {
+  blockStandartAction(event)
+
+  if(event.target.nodeName !== 'IMG'){
+      return;
+  }
+
+ const galleryNew = new SimpleLightbox('.gallery a' , {
+    captionsData: 'alt',
+ });
+ }
+//   galleryNew.on('show.simplelightbox', function () {
+// `<img src="${event.target.href}" width="800" height="600" >`
+//   });
+
+
+// gallery.addEventListener('keydown', (event)=>{ 
+//   if(event.code === 'Escape'){
+//     instance.close()
+//   }
+// }) 
+// }
+
+function blockStandartAction(event){
+  event.preventDefault();
+}
